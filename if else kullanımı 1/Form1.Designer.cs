a5a698e621c39bea148cb3d769e1c16e5ee3ade8
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-09 12:35:05 +0300

    Add files via upload

---
+++ if else kullanımı 1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace if_else_kullanımı_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRenk = new System.Windows.Forms.Button();
+            this.cbMavi = new System.Windows.Forms.CheckBox();
+            this.lblYaz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRenk
+            // 
+            this.btnRenk.Location = new System.Drawing.Point(15, 104);
+            this.btnRenk.Name = "btnRenk";
+            this.btnRenk.Size = new System.Drawing.Size(91, 28);
+            this.btnRenk.TabIndex = 0;
+            this.btnRenk.Text = "Renk Seç";
+            this.btnRenk.UseVisualStyleBackColor = true;
+            this.btnRenk.Click += new System.EventHandler(this.btnRenk_Click);
+            // 
+            // cbMavi
+            // 
+            this.cbMavi.AutoSize = true;
+            this.cbMavi.Location = new System.Drawing.Point(12, 33);
+            this.cbMavi.Name = "cbMavi";
+            this.cbMavi.Size = new System.Drawing.Size(49, 17);
+            this.cbMavi.TabIndex = 1;
+            this.cbMavi.Text = "Mavi";
+            this.cbMavi.UseVisualStyleBackColor = true;
+            // 
+            // lblYaz
+            // 
+            this.lblYaz.AutoSize = true;
+            this.lblYaz.Location = new System.Drawing.Point(12, 73);
+            this.lblYaz.Name = "lblYaz";
+            this.lblYaz.Size = new System.Drawing.Size(0, 13);
+            this.lblYaz.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 166);
+            this.Controls.Add(this.lblYaz);
+            this.Controls.Add(this.cbMavi);
+            this.Controls.Add(this.btnRenk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRenk;
+        private System.Windows.Forms.CheckBox cbMavi;
+        private System.Windows.Forms.Label lblYaz;
+    }
+}
+
---
+++ if else kullanımı 1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_else_kullanımı_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRenk_Click(object sender, EventArgs e)
+        {
+            if (cbMavi.Checked == true) 
+            {
+                lblYaz.Text = "Mavi yapıldı.";
+                BackColor = Color.CornflowerBlue;
+            }
+            else
+            {
+                lblYaz.Text = "Mavi kaldırıldı.";
+                BackColor = Color.White;
+            }
+        }
+    }
+}
